11c837417a25a9da630d264f275797d4631c3a7d
Matt Fisher <matt.fisher@burningman.org>
2024-06-06 11:24:55 -0700

    scaffold

---
+++ bernie-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5bdf7ec68b3798980a2b');
---
+++ bernie-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1081fb4ca346497ec07b');
